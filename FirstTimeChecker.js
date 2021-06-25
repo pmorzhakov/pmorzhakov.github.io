1e32a5805a25a1c8b12c8133cdfe91e4c6acdbb0
pmorzhakov <55410417+pmorzhakov@users.noreply.github.com>
2021-06-25 16:46:10 +0300

    Initial Commit

---
+++ FirstTimeChecker.js
@@ -0,0 +1,21 @@
+
+if (! localStorage.noFirstVisit) {
+    console.log('first time');
+    document.getElementById('first').style.display = 'block';
+    localStorage.noFirstVisit = "1";
+} else {
+  if(localStorage.noFirstVisit == "1"){
+    document.getElementById('wrap').style.display = 'block';
+    localStorage.noFirstVisit = "2";
+  } else {
+    document.getElementById('last').style.display = 'block';
+  }
+    
+}
+  
+
+
+document.getElementById('restore').onclick = function () {
+    localStorage.noFirstVisit = "";
+    document.getElementById('restore').innerHTML = "DO NOT FORGET TO RUN AGAIN THE PAGE";
+};
