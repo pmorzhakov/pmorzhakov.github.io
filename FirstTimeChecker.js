43452bdcb4eddcd5e97a2adc64cd3cb0f58a7a55
pmorzhakov <55410417+pmorzhakov@users.noreply.github.com>
2021-06-25 16:59:09 +0300

    wrap FTC in window.onload

--- FirstTimeChecker.js
+++ FirstTimeChecker.js
@@ -1,21 +1,22 @@
+window.onload = function () {
+    if (! localStorage.noFirstVisit) {
+        console.log('first time');
+        document.getElementById('first').style.display = 'block';
+        localStorage.noFirstVisit = "1";
+    } else {
+      if(localStorage.noFirstVisit == "1"){
+        document.getElementById('wrap').style.display = 'block';
+        localStorage.noFirstVisit = "2";
+      } else {
+        document.getElementById('last').style.display = 'block';
+      }
 
-if (! localStorage.noFirstVisit) {
-    console.log('first time');
-    document.getElementById('first').style.display = 'block';
-    localStorage.noFirstVisit = "1";
-} else {
-  if(localStorage.noFirstVisit == "1"){
-    document.getElementById('wrap').style.display = 'block';
-    localStorage.noFirstVisit = "2";
-  } else {
-    document.getElementById('last').style.display = 'block';
-  }
-    
+    }
+
+
+
+    document.getElementById('restore').onclick = function () {
+        localStorage.noFirstVisit = "";
+        document.getElementById('restore').innerHTML = "DO NOT FORGET TO RUN AGAIN THE PAGE";
+    };
 }
-  
-
-
-document.getElementById('restore').onclick = function () {
-    localStorage.noFirstVisit = "";
-    document.getElementById('restore').innerHTML = "DO NOT FORGET TO RUN AGAIN THE PAGE";
-};
